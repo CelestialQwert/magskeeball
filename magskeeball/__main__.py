d9707a66777e17dd64904ac4d43ccb0dc92c314d
CelestialQwert <celestialqwert@gmail.com>
2024-03-18 21:28:36 -0400

    Rework packaging and some tests

---
+++ magskeeball/__main__.py
@@ -0,0 +1,15 @@
+import argparse
+import sys
+
+choices = ['game', 'sensor_test']
+
+parser = argparse.ArgumentParser()
+parser.add_argument('test_mode', choices=choices)
+test_mode = parser.parse_args().test_mode
+
+if test_mode == 'sensor_test':
+    from .import sensor
+    sensor.main()
+else:
+    from . import main
+    main.run()
