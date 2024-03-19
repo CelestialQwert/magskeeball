89f6cfaea6197f1ab7f2d226d8234321ef69090b
CelestialQwert <celestialqwert@gmail.com>
2024-03-18 21:32:14 -0400

    Fix up module arguments

--- magskeeball/__main__.py
+++ magskeeball/__main__.py
@@ -4,7 +4,9 @@ import sys
 choices = ['game', 'sensor_test']
 
 parser = argparse.ArgumentParser()
-parser.add_argument('test_mode', choices=choices)
+parser.add_argument(
+    'test_mode', choices=choices, default='game', const='game', nargs='?'
+)
 test_mode = parser.parse_args().test_mode
 
 if test_mode == 'sensor_test':
